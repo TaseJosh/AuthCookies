d0637dc5f5de7e1aed16b0c3c438c70a67087bff
Erutase Akpobome <erutase.akpobome@liverpoolft.nhs.uk>
2021-08-23 08:58:08 +0100

    Add project files.

---
+++ AuthCookies/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using AuthCookies.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace AuthCookies.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login (string username, string password, string ReturnUrl)
+        {
+            if (username=="tase" && password == "123")
+            {
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, username)
+                };
+
+                var claimsIdentity = new ClaimsIdentity(claims, "Login");
+
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
+                return Redirect(ReturnUrl == null ? "/Students" : ReturnUrl);
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+      [HttpPost]
+      public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync();
+            return RedirectToAction("Index");
+        }
+    }
+}
